3aad77990f1e8bb696da88cf3cb03502148001cd
JensHoffmann <jens.hoffmann@cuesoft.eu>
2014-02-10 13:13:20 +0100

    Euler problem 35

--- euler/euler035/euler035.cc
+++ euler/euler035/euler035.cc
@@ -11,64 +11,47 @@
 // How many circular primes are there below one million?
 //
 #include <stdio.h>
-#include <unordered_map>
-
-std::unordered_map<int, bool> primes;
-
-bool isPrime(int n) {
-  bool result = true;
-  if (primes.count(n)) {
-    result = true;
-  } else {
-    for (int i = 2; i*i <= n; ++i) {
-      if (n % i == 0) {
-        result = false;
-        break;
-      }
-    }
-    if (result) {
-      primes[n] = true;
-    }
-  }
-
-  return result;
-}
-
-int rotate(int n) {
-  int result = 0;
-  int factor = 10;
-  while (n >= 10) {
-    result += (n % 10) * factor;
-    n /= 10;
-    factor *= 10;
-  }
-  result += n;
-
-  return result;
-}
-
-bool isCircularPrime(int n) {
-  bool result = true;
-  int nOriginal = n;
-  do {
-    if (!isPrime(n)) {
-      result = false;
-      break;
-    } else {
-    }
-    n = rotate(n);
-  } while (n != nOriginal);
-
-  return result;
-}
+#include "../prime.h"
 
 int main() {
-  int count = 0;
-  for (int i = 2; i < 1000*1000; ++i) {
-    if (isCircularPrime(i)) {
-        count += 1;
+  int count = 1;
+  for (int x = 3; x < 1000*1000; x += 2) {
+    int x2 = x;
+    bool isCircularPrime = true;
+    int k = 0;
+    while (x2) {
+      x2 /= 10;
+      k += 1;
+    }
+    x2 = x;
+    do {
+      if (!utils::isPrime(x2)) {
+        isCircularPrime = false;
+        break;
+      }
+      int xc = x2 % 10;
+      x2 /= 10;
+      int xr = 0;
+      while (x2) {
+        xr *= 10;
+        xr += x2 % 10;
+        x2 /= 10;
+      }
+      int k2 = k;
+      while (k2 - 1) {
+        xc *= 10;
+        xc += xr % 10;
+        xr /= 10;
+        k2 -= 1;
+      }
+      x2 = xc;
+    } while (x2 != x);
+    if (isCircularPrime) {
+      printf("%d is circular prime\n", x);
+      count += 1;
     }
   }
+  printf("\n");
   printf("There are %d circular primes below one million.\n", count);
 
   return 0;
